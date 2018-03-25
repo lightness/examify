d43c803f03b4cc4758d150989aa1c55c051b06c4
vasaeps3 <vasaeps3@gmail.com>
2018-03-25 17:11:41 +0300

    fix using font-awesome and bootstrap add ng-bootstrap fix header menu

--- frontend/examify-frontend/src/app/menu/menu.module.ts
+++ frontend/examify-frontend/src/app/menu/menu.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { MenuComponent } from "./menu.component";
 
@@ -10,7 +11,8 @@ import { MenuComponent } from "./menu.component";
     imports: [
         CommonModule,
         FormsModule,
-        RouterModule
+        RouterModule,
+        NgbDropdownModule.forRoot()
     ],
     declarations: [
         MenuComponent,
